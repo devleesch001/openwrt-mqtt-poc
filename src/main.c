35639651ee8d4e91c665599603f759561d7617c7
Alexis DEVLEESCHAUWER <a.devleeschauwer@pyres.com>
2023-10-27 11:00:30 +0200

    feat use mosqutto in code

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+#include <mosquitto.h>
 
 int main(int argc, char **argv)
 {
     printf("Hello, World!\n");
+
+	int major = 0, minor = 0, revision = 0;
+    mosquitto_lib_version(&major, &minor, &revision);
+
+    printf("libmosquitto version %d.%d.%d\n", major, minor, revision);
 
 	return 0;
 }
